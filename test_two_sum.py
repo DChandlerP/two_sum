6c082dfc8c2685a1e925a263b97d9576eed5e9f9
David Prince <davidprince@Davids-MBP.attlocal.net>
2021-07-16 17:50:58 -0500

    revised example

--- test_two_sum.py
+++ test_two_sum.py
@@ -1,6 +1,10 @@
 import two_sum
 
-class Test_Two_sum_Two_sum:
-    def test_two_sum_1(self):
-        result = two_sum.two_sum([0, 1, 2, 8, 22], 10)
+class Test_Two_sum_TwoSum:
+    def test_twoSum_1(self):
+        result = two_sum.twoSum([0, 1, 2, 8, 22], 10)
         assert result == [2, 8]
+
+    def test_twoSum_2(self):
+        result = two_sum.twoSum([0, 0], 3)
+        assert result == []
--- two_sum.test.py
+++ two_sum.test.py
@@ -2,5 +2,9 @@ import two_sum
 
 class Test_Two_sum_TwoSum:
     def test_twoSum_1(self):
-        result = two_sum.twoSum([0, 2, 8, 11], 10)
-        assert result == [1, 2]
+        result = two_sum.twoSum([0, 1, 2, 8, 22], 10)
+        assert result == [2, 3]
+
+    def test_twoSum_2(self):
+        result = two_sum.twoSum([0, 0], 3)
+        assert result == []
